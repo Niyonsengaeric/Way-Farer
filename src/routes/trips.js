10798b250377d6c6a00e2faf1d4b59e4751f948f
Believe <niyeric11@.com>
2019-07-31 18:41:43 +0200

    feature(#167430856):Get trips finishes[#167430856]

--- src/routes/trips.js
+++ src/routes/trips.js
@@ -5,11 +5,11 @@ import auth from "../middlewares/auth";
 import admin from "../middlewares/admin";
 const router = express.Router();
 
-// router.post("/trips", [auth, admin], regTrip);
-// router.patch("/trips/:id/cancel", [auth, admin], cancelTrip);
-// router.get("/trips", auth, getTrips);
+router.post("/trips", [auth, admin], regTrip);
+router.patch("/trips/:id/cancel", [auth, admin], cancelTrip);
+router.get("/trips", auth, getTrips);
 // router.get("/trips/:id", auth, spfTrip);
 
-// '/', auth
+// '/',
 
 export default router;
