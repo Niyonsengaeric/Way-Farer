f7d460556760a59a1578566e4d0c63f3ac08c32f
Niyonsengaeric <50768983+Niyonsengaeric@users.noreply.github.com>
2019-08-14 10:28:00 +0000

    #167829247 book-trip (#57)
    * book-trip
    [Finishes #167829247]
    
    * remove-typo-error
    
    * declare-variableBeforeUsing
    
    * remove-Lintingerror
    
    * updating-lintingScript
    
    * ubject-destructuring
    
    * objectDestructing

--- server/controllers/bookingControllers.js
+++ server/controllers/bookingControllers.js
@@ -1,184 +1,145 @@
 import moment from 'moment';
+import { Client } from 'pg';
+import dotenv from 'dotenv';
 import validate from '../middlewares/validateBooking';
 import response from '../helpers/response';
-//Database connection
-import {Client, Pool} from 'pg';
-import dotenv from 'dotenv';
+// Database connection
+
 dotenv.config();
-const{JWT} = process.env;
-const {DATABASE_URL} = process.env;
+const { DATABASE_URL } = process.env;
 const connectionString = DATABASE_URL;
 const client = new Client({
-  connectionString
+  connectionString,
 });
-client.connect()
-
+client.connect();
 
 class bookingsController {
   // view all properties
-  static async book(req, res) {  
-  const { error } = validate(req.body);
-  if (error) {
-    return response.response(res, 422,'error', `${error.details[0].message}`, true);
-
-  }
-
-  // // ###check trip ID
-  const tripid = req.body.tripId;
-  let findtripid = await client.query('SELECT * FROM trips WHERE id=$1',[
-    req.body.tripId,
-  ]);
-  if (findtripid.rows.length <=0) 
-  { 
-    
-   return response.response(res, 404,'error', 'No trip found!', true);
- }
-  if (findtripid) 
-  { 
-      // check if trip is activated*
-    if (findtripid.rows[0].status == 'CANCELED') {
-      return response.response(
-        res,
-        406,'error',
-  
-        'TRIP HAS BEEN CANCELED!!! PLEASE TRY ANOTHER DIFFERENT TRIP',
-        true
-      );
+  static async book(req, res) {
+    const { error } = validate(req.body);
+    if (error) {
+      return response.response(res, 422, 'error', `${error.details[0].message}`, true);
     }
 
-
- }
- 
-  const buslicence = findtripid.rows[0].bus_license_number;
-  const origin  = findtripid.rows[0].origin;
-  const destination  = findtripid.rows[0].destination;
-  const fare = findtripid.rows[0].fare;
-  const triptime = findtripid.rows[0].time;
-  const tripidnumber = findtripid.rows[0].id;
-  const tripdate = findtripid.rows[0].trip_date;
-  const seating_capacity = findtripid.rows[0].seating_capacity;
-
-
-  // // ###check users  req.user.id
-  let userCheck = await client.query('SELECT * FROM users WHERE id=$1 ',[
-    req.user.id,
-  ]);
-  const userid = req.user.id;
-  // geting user info
-
-  const firstname = userCheck.rows[0].first_name;
-  const lastname = userCheck.rows[0].last_name;
-  const useremail = userCheck.rows[0].email;
-  const userphone = userCheck.rows[0].phonenumber;
- 
-
-  // check for booking and save
-  if (seating_capacity <= 0) {
-    return response.response(
-      res,
-      406,'error',
-      'SORRY!!! No seats left on the trip',
-      true
-    );
-    
-  }
-
-
-  let book = await client.query('SELECT * FROM bookings WHERE trip_id=$1 AND user_id=$2',[
-    req.body.tripId,req.user.id,
-  ]);
-
-  if (!book.rows[0]) {
-    // ##### inserting in table
-
-    let recordbooking = client.query('INSERT INTO bookings(booking_date, first_name,last_name, phonenumber, user_email, bus_license,origin,destination,trip_date,time,fare,trip_id,user_id)VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)',[
-      moment().format(), firstname,lastname, userphone,useremail,buslicence,origin,destination,tripdate ,triptime,fare,tripidnumber,userid,
+    // // ###check trip ID
+    const tripid = req.body.tripId;
+    const findtripid = await client.query('SELECT * FROM trips WHERE id=$1', [
+      tripid,
     ]);
-    if (recordbooking ) {
+    if (findtripid.rows.length <= 0) {
+      return response.response(res, 404, 'error', 'No trip found!', true);
+    }
+    if (findtripid) {
+      // check if trip is activated*
+      if (findtripid.rows[0].status === 'CANCELED') {
+        return response.response(res, 406, 'error', 'TRIP HAS BEEN CANCELED!!! PLEASE TRY ANOTHER DIFFERENT TRIP', true);
+      }
+    }
+
+    const {
+      bus_license_number, origin, destination, fare, time, id, trip_date, seating_capacity,
+    } = findtripid.rows[0];
+
+
+    // // ###check users  req.user.id
+    const { id: userid } = req.user;
+    const userCheck = await client.query('SELECT * FROM users WHERE id=$1 ', [
+      userid,
+    ]);
+
+    // geting user info
+    const {
+      first_name, last_name, email, phonenumber,
+    } = userCheck.rows[0];
+
+    // check for booking and save
+
+    if (seating_capacity <= 0) { return response.response(res, 406, 'error', 'SORRY!!! No seats left on the trip', true); }
+    const book = await client.query('SELECT * FROM bookings WHERE trip_id=$1 AND user_id=$2', [
+      tripid, userid,
+    ]);
+
+    if (!book.rows[0]) {
+    // ##### inserting in table
+      const cols = 'booking_date, first_name,last_name, phonenumber, user_email, bus_license,origin,destination,trip_date,time,fare,trip_id,user_id';
+      const cels = '$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13';
+      const data = [
+        moment().format(), first_name, last_name, phonenumber, email, bus_license_number, origin, destination, trip_date, time, fare, id, userid,
+      ];
+      const recordbooking = client.query(
+        `INSERT INTO bookings(${cols})VALUES(${cels})`, data,
+      );
+      if (recordbooking) {
       // ###update trip seats
-        let updatetrip = client.query('UPDATE trips SET seating_capacity=$1 where id = $2',[
-          seating_capacity-1,findtripid.rows[0].id,
-        ])
-
-        const book= {
+        const updatetrip = client.query('UPDATE trips SET seating_capacity=$1 where id = $2', [
+          seating_capacity - 1, findtripid.rows[0].id,
+        ]);
+        if (!updatetrip) { return response.response(res, 500, 'error', 'Error running query!', true); }
+        const getbook = {
           booking_date: moment().format(),
-          first_name: firstname.toUpperCase(),
-          last_name: lastname.toUpperCase(),
-          phonenumber: userphone,
-          user_email: useremail.toLowerCase(),
-          bus_license_number: buslicence.toUpperCase(),
-          origin: origin.toUpperCase(),
-          destination: destination.toUpperCase(),
-          trip_date: tripdate,
-          trip_time: triptime,
-          farer:fare,
-        }
-
-
-        return response.response(res, 201,'success', book, false);
-  
+          first_name,
+          last_name,
+          phonenumber,
+          email,
+          bus_license_number,
+          origin,
+          destination,
+          trip_date,
+          time,
+          fare,
+        };
+        return response.response(res, 201, 'success', getbook, false);
+      }
+    } else {
+      return response.response(res, 409, 'error', 'booking already made!', true);
     }
   }
 
-else {
-    return response.response(res, 409,'error', 'booking already made!', true);
-  }
-};
+
+  static async getbookings(req, res) {
+  // ###Display all bookings made by users //as admin
+    if (req.user.is_admin) {
+      client.query('SELECT * FROM bookings', (err, result) => {
+        if (err) {
+          return response.response(res, 404, 'error', 'Error running query');
+        }
+        const resul = result.rows;
+        return response.response(res, 200, 'success', resul, false);
+      });
+    }
+    // ###Display bookings  user for user only
+    else {
+      const finduserid = await client.query('SELECT * FROM bookings WHERE user_id=$1', [
+        req.user.id,
+      ]);
 
 
-
-static async getbookings(req, res) {
-  // ###Display all bookings made by users //sa admin
-  if (req.user.is_admin)
-  {
-    client.query('SELECT * FROM bookings', function(err, result){
-      if (err){
-        return response.response(res, 404,'error', 'Error running query');
-      }else{
-        let resul = result.rows;
-        return response.response(res,200,'success',resul,false);      
+      if (finduserid.rows.length > 0) {
+        return response.response(res, 200, 'success', finduserid.rows, false);
       }
-    })    
+      return response.response(res, 404, 'error', 'no bookings found', true);
+    }
   }
-  // ###Display bookings  user for user only
-  else{
-  let finduserid = await client.query('SELECT * FROM bookings WHERE user_id=$1',[
-    req.user.id,
-  ]);
 
-  
-  if(finduserid.rows.length>0){ 
-    return response.response(res, 200,'success', finduserid.rows, false);
-    
+
+  static async deletebooking(req, res) {
+    const { id } = req.params;
+
+    const findbook = await client.query('SELECT * FROM bookings WHERE id=$1 AND user_id=$2', [
+      parseInt(id, 10), req.user.id,
+    ]);
+
+    // ###delete a Booking
+    if (findbook.rows.length > 0) {
+      const recordprop = client.query('DELETE FROM bookings WHERE id =$1', [
+        parseInt(id, 10),
+      ]);
+      if (recordprop) {
+        return response.response(res, 200, 'success', 'Booking deleted successfully', false);
+      }
+    } else {
+      return response.response(res, 404, 'error', 'Booking not Found!', true);
+    }
   }
-  return response.response(res, 404,'error', 'no bookings found', true);
-}    
-
- 
-};
-
-
-static async deletebooking(req, res) {
-  const { id } = req.params;
-
-  let findbook = await client.query('SELECT * FROM bookings WHERE id=$1 AND user_id=$2',[
-    parseInt(id, 10),req.user.id,
-  ]);
-
-  //###delete a Booking
-  if(findbook.rows.length>0){  
-    
-  let recordprop = client.query('DELETE FROM bookings WHERE id =$1',[
-    parseInt(id, 10)
-   ]);
-   if (recordprop){
-    return response.response(res, 200, 'success', 'Booking deleted successfully', false);
-   }
-
-  }
- else {
-    return response.response(res, 404,'error', 'Booking not Found!', true);
-  }
-};
 }
 export default bookingsController;
--- server/controllers/tripsControllers.js
+++ server/controllers/tripsControllers.js
@@ -1,103 +1,100 @@
-import validate from '../middlewares/validateTrip';
-import express from 'express';
-import response from '../helpers/response';
-const router = express.Router();
-//Database connection
-import {Client, Pool} from 'pg';
+
+import { Client } from 'pg';
 import dotenv from 'dotenv';
+import response from '../helpers/response';
+import validate from '../middlewares/validateTrip';
+
+
 dotenv.config();
-const {DATABASE_URL} = process.env;
+const { DATABASE_URL } = process.env;
 const connectionString = DATABASE_URL;
 const client = new Client({
-  connectionString
+  connectionString,
 });
-client.connect()
+client.connect();
 
 
 class tripscontrolllers {
-  static async  regTrip(req, res) {
-  const { error } = validate(req.body);
-  if (error)
-    return response.response(res, 422,'error', `${error.details[0].message}`, true);
+  static async regTrip(req, res) {
+    const { error } = validate(req.body);
+    if (error) { return response.response(res, 422, 'error', `${error.details[0].message}`, true); }
 
-    let trip_origin = await client.query('SELECT * FROM trips WHERE origin=$1 ',[
+    let trip_origin = await client.query('SELECT * FROM trips WHERE origin=$1 ', [
       req.body.origin,
     ]);
 
-    let trip_destination = await client.query('SELECT * FROM trips WHERE destination=$1 ',[
+    let trip_destination = await client.query('SELECT * FROM trips WHERE destination=$1 ', [
       req.body.destination,
     ]);
 
-    let trip_date = await client.query('SELECT * FROM trips WHERE trip_date=$1 ',[
+    let trip_date = await client.query('SELECT * FROM trips WHERE trip_date=$1 ', [
       req.body.trip_date,
     ]);
 
-    let trip_time = await client.query('SELECT * FROM trips WHERE time=$1 ',[
+    let trip_time = await client.query('SELECT * FROM trips WHERE time=$1 ', [
       req.body.time,
     ]);
 
 
-  if (trip_origin.rows.length > 0 && trip_destination.rows.length > 0 && trip_date.rows.length > 0 && trip_time.rows.length > 0 )
-   {
-    return response.response(res, 409,'error', 'trip already registered ', true);
-
-    
-  } else {
-
-    let recordTrip = client.query('INSERT INTO trips(seating_capacity, bus_license_number, origin, destination, trip_date, fare,status, time)VALUES($1,$2,$3,$4,$5,$6,$7,$8)',[
-      req.body.seating_capacity, req.body.bus_license_number, req.body.origin, req.body.destination, req.body.trip_date, req.body.fare, 'ACTIVE' ,req.body.time,
-    ]); 
-    if (recordTrip){ 
-      const{ seating_capacity,bus_license_number,origin,destination,trip_date,fare,time} =req.body;      
-      const payload= { seating_capacity,bus_license_number,origin,destination,trip_date,fare,time }
-      return response.response(res, 201,'success', payload, false);
-     }
-
-  }
-};
-static async  cancelTrip(req, res) {
-
-  let trip_id = await client.query('SELECT * FROM trips WHERE id=$1',[
-    req.params.id,
-  ]);
-  if(trip_id.rows.length>0){ 
-    console.log(trip_id.rows[0].status)
-    if (trip_id.rows[0].status=='CANCELED')
+    if (trip_origin.rows.length > 0 && trip_destination.rows.length > 0 && trip_date.rows.length > 0 && trip_time.rows.length > 0)
     {
-      return response.response(res, 406,'error', 'trip Already CANCELED!', true);
+      return response.response(res, 409, 'error', 'trip already registered ', true);
+    }
+
+    let recordTrip = client.query('INSERT INTO trips(seating_capacity, bus_license_number, origin, destination, trip_date, fare,status, time)VALUES($1,$2,$3,$4,$5,$6,$7,$8)', [
+      req.body.seating_capacity, req.body.bus_license_number, req.body.origin, req.body.destination, req.body.trip_date, req.body.fare, 'ACTIVE', req.body.time,
+    ]);
+    if (recordTrip) {
+      const {
+        seating_capacity, bus_license_number, origin, destination, trip_date, fare, time,
+      } = req.body;
+      const payload = {
+        seating_capacity, bus_license_number, origin, destination, trip_date, fare, time,
+      };
+      return response.response(res, 201, 'success', payload, false);
+    }
+  }
+
+  static async cancelTrip(req, res) {
+    let trip_id = await client.query('SELECT * FROM trips WHERE id=$1', [
+      req.params.id,
+    ]);
+    if (trip_id.rows.length > 0) {
+      console.log(trip_id.rows[0].status);
+      if (trip_id.rows[0].status === 'CANCELED')
+      {
+        return response.response(res, 406, 'error', 'trip Already CANCELED!', true);
       }
-    let updatetrip = client.query('UPDATE trips SET status=$1 where id = $2',[
-      'CANCELED',req.params.id,
-    ])
-    trip_id.rows[0].status = 'CANCELED';
-      return response.response(res, 200,'success', 'Trip cancelled successfully', false);
+      let updatetrip = client.query('UPDATE trips SET status=$1 where id = $2', [
+        'CANCELED', req.params.id,
+      ]);
+      if (!updatetrip) { return response.response(res, 500, 'error', 'Error running query!', true); }
+      trip_id.rows[0].status = 'CANCELED';
+      return response.response(res, 200, 'success', 'Trip cancelled successfully', false);
     }
- else {
-    return response.response(res, 404,'error', 'Trip not Found!', true);
+
+    return response.response(res, 404, 'error', 'Trip not Found!', true);
   }
-};
 
 
-static async  getTrips(req, res) {
-  client.query('SELECT * FROM trips', function(err, result){
-    if (result){ let resul = result.rows;
-      return response.response(res,200,'success',resul,false);   
+  static async getTrips(req, res) {
+    client.query('SELECT * FROM trips', (err, result) => {
+      if (result) { let resul = result.rows;
+        return response.response(res, 200, 'success', resul, false);
+      }
+    });
+  }
+
+  static async spfTrip(req, res) {
+    const { id } = req.params;
+    let searchtrip = await client.query('SELECT * FROM trips WHERE id=$1', [
+      id,
+    ]);
+    if (searchtrip.rows.length > 0) {
+      return response.response(res, 200, 'success', searchtrip.rows, false);
     }
-  })
 
-
+    return response.response(res, 404, 'error', 'Trip not Found!', true);
   }
-static async  spfTrip(req, res) {
-  const { id } = req.params;
-  let searchtrip = await client.query('SELECT * FROM trips WHERE id=$1',[
-    req.params.id,
-  ]);
-  if(searchtrip.rows.length>0){
-    return response.response(res, 200,'success', searchtrip.rows,false); 
-}
- else {
-    return response.response(res, 404,'error', 'Trip not Found!', true);
-  }
-};
 }
 export default tripscontrolllers;
